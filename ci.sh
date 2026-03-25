57c571fc44fa08e708da879aa0a3e218b016370e
Daniel Stuart <daniel.stuart14@gmail.com>
2026-03-24 21:38:58 -0300

    v1.3.0 release

--- ci.sh
+++ ci.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 cargo test
-cargo build --target thumbv6m-none-eabi
+cargo build --features _enable_critical_section --target thumbv6m-none-eabi
 cargo build --target thumbv7em-none-eabi
